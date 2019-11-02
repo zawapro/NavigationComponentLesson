96b3a6caf641d020bca1ea85f38286b0e3cfbab1
zawatan <utwdev@gmail.com>
2019-11-02 12:09:29 +0900

    Fragmentを追加

---
+++ app/src/main/java/com/zawapro/navigationcomponentlesson/MyFragment2.kt
@@ -0,0 +1,17 @@
+package com.zawapro.navigationcomponentlesson
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MyFragment2 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_fragment2, container, false)
+    }
+}
