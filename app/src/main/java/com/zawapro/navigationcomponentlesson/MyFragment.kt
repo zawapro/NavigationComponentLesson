3aec42f71d1271bc99a03bbf5742886448dd116a
zawatan <utwdev@gmail.com>
2019-11-02 12:25:21 +0900

    ボタンクリックイベント、遷移を追加

--- app/src/main/java/com/zawapro/navigationcomponentlesson/MyFragment.kt
+++ app/src/main/java/com/zawapro/navigationcomponentlesson/MyFragment.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
-
+import androidx.navigation.fragment.findNavController
 
 class MyFragment : Fragment() {
     override fun onCreateView(
@@ -13,6 +14,11 @@ class MyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my, container, false)
+        return inflater.inflate(R.layout.fragment_my, container, false).also { layout ->
+            layout.findViewById<Button>(R.id.button).setOnClickListener {
+                // NavControllerを呼び出し、アクションを指定して遷移する
+                findNavController().navigate(R.id.action_myFragment_to_myFragment22)
+            }
+        }
     }
 }
